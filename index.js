21b958084e640bf783195d0fb097d26d3a1c4f79
Rodolfo De Chiara <rodolfo.dechiara@tuta.io>
2022-05-20 20:47:58 +0200

    feat: allow user to set grid size

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+let gridSize = 16;
+
+function setGridSize()
+{
+    document.getElementById('container').innerHTML = '';
+    do {
+        gridSize = Number(prompt('Grid size (<= 100): ', '16'));
+    }
+    while(gridSize > 100 || isNaN(gridSize));
+    initializeGrid();
+}
 function initializeGrid()
 {
     const container = document.getElementById('container');
     container.classList.add('container');
 
-    for(let i = 0; i < 16; i++)
+    for(let i = 0; i < gridSize; i++)
     {
         let row = document.createElement('div');
         row.classList.add('row');
-        for(let j = 0; j < 16; j++)
+        for(let j = 0; j < gridSize; j++)
         {
             let cell = document.createElement('div');
             cell.style.background =  `rgb(0, 0, 0)`;
@@ -20,5 +31,3 @@ function initializeGrid()
         container.append(row);
     }
 }
-
-initializeGrid();
